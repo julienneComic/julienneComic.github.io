386b827e61d7da1cc0a8572e154efbee09c5e5db
julienneComic <julienne.comic@proton.me>
2024-05-09 22:23:33 -0700

    finishes archive page
    adds colors displays all the pages

--- src/components/archive/index.js
+++ src/components/archive/index.js
@@ -7,15 +7,43 @@ class Archive extends HTMLElement {
     this.innerHTML = `
 <style>
 content {
+  margin: 28px auto;
+  max-width: 981px;
   padding: 20px 0;
   display: block;
 }
-h2 {
+content h2 {
   padding: 0px 20px 20px 20px;
 }
-details {
-  padding: 20px;
+content details {
+  background-color: #d0fffe;
+  margin: 16px;
+  padding-bottom: 20px;
 }
+content summary {
+  display: block;
+  curosr: pointer;
+  padding: 20px 20px 0 20px;
+}
+content ul {
+  display: flex;
+  flex-wrap: wrap;
+}
+content li {
+  width: 80px;
+  padding: 8px;
+  margin: 8px;
+  text-align: center;
+  background-color: #b1f9f9;
+  transition: background-color 250ms;
+}
+content li:hover {
+  background-color: white;
+}
+a:link    {color:#000;}  /* unvisited link  */
+a:visited {color:#000;}  /* visited link    */
+a:hover   {color:#000;}  /* mouse over link */
+a:active  {color:#000;} 
 </style>
 <content id="content">
   <h2>Archive</h2>
@@ -24,7 +52,7 @@ details {
   }
 }
 
-customElements.define("about-component", Archive);
+customElements.define("archive-component", Archive);
 const setupArchivePage = async () => {
   const content = document.getElementById("content");
   const response = await fetch(`./src/data/archive.json`);
@@ -36,15 +64,17 @@ const setupArchivePage = async () => {
 
     connectedCallback() {
       this.innerHTML = `
+<style>
+</style>
   <details>
-  <summary></summary>
-  <ul></ul>
+    <summary><h3></h3></summary>
+    <ul></ul>
   </details>
   `;
     }
   }
   customElements.define("archive-row-component", ArchiveRow);
-
+  let postNumber = 1;
   const chapterNumbers = Object.keys(body);
   for (const chapterNumber of chapterNumbers) {
     content.insertAdjacentHTML(
@@ -53,19 +83,24 @@ const setupArchivePage = async () => {
 <archive-row-component id="chapter-${chapterNumber}"></archive-row-component>
 `,
     );
-    const [summary, pageChildren] = document.getElementById(
-      `chapter-${chapterNumber}`,
-    ).children[0].children;
-    summary.innerText = `Chapter ${chapterNumber}`;
+    console.log(document.getElementById(`chapter-${chapterNumber}`));
+    const [summary, pageChildren] = document
+      .getElementById(`chapter-${chapterNumber}`)
+      .getElementsByTagName("details")[0].children;
+    // console.log(details);
+    // const summary = details.firstChild;
+    // const pageChildren = details.lastChild;
+    summary.children[0].innerText = `Chapter ${chapterNumber}`;
     for (const pageNumber of body[chapterNumber]) {
       pageChildren.insertAdjacentHTML(
         "beforeend",
         `
 <li>
-  <a href="./?view=page&page=${pageNumber}">Chapter ${chapterNumber}: Page ${pageNumber}</a>
+  <a href="./?view=page&page=${postNumber}">Page ${pageNumber}</a>
 </li>
 `,
       );
+      postNumber += 1;
     }
   }
 };
