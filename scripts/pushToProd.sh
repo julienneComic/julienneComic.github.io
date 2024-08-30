917d4668a522cfbe7333185e4338fa7d4d52fdfb
1like2learn <pear124@pm.me>
2024-08-29 20:42:38 -0700

    adds push to prod script

--- scripts/pushToProd.sh
+++ scripts/pushToProd.sh
@@ -2,30 +2,21 @@
 
 git add .
 git diff --name-only --cached
-
-echo "\nDo you want to push these changes?"
-select yn in "Yes" "No"; do
-    case $yn in
-        Yes ) break;;
-        No ) echo "\nPlease remove changes to files you don't want pushed"; exit;;
-    esac
-done
-
-echo "\nWhat is the purpose of this push"
-read commitMessage
-while true; do
-    read -p "Do you wish to install this program? " yn
-    case $yn in
-        [Yy]* ) make install; break;;
-        [Nn]* ) exit;;
-        * ) echo "Please answer yes or no.";;
-    esac
-done
-
-git commit -m "$commitMessage"
-
-echo "\nEnter pushes to production or N to push to test."
-
+echo;
+statusCmd="git status --short";
+status="$statusCmd";
+if [ "$status" != "\n" ]; then
+  echo "\nDo you want to push these changes?"
+  select yn in "Yes" "No"; do
+      case $yn in
+          Yes ) break;;
+          No ) echo "\nPlease remove changes to files you don't want pushed"; exit;;
+      esac
+  done;
+  read -p "\nWhat is the purpose of this push? " commitMessage;
+  git commit -m "$commitMessage";
+fi
+read -p "\nEnter deploys to production or N to push to test." stageChanges;
 if ["$stageChanges" == "N" | "$stageChanges" == "n"]; then
   git push origin HEAD
   exit 0
