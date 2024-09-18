03990c0520cab33d25f184c02769265dec88a3dd
1like2learn <pear124@pm.me>
2024-09-17 20:20:47 -0700

    updates about text

--- src/components/about/index.js
+++ src/components/about/index.js
@@ -56,42 +56,28 @@ about-component img#portrait {
 </style>
 <section>
   <div class="container">
-    <h3>About the Comic</h3>
+    <h3>ABOUT THE COMIC</h3>
+    <p>When he was thirteen years old, Julienne accidentally killed his best friend’s father in self-defense. Fueled by the stigma surrounding his siren-like species, the courts imprisoned him until, many years later, his estranged best friend has managed to negotiate his acquittal.</p>
     <br />
-    <h4>When he was thirteen years old,</h4>
-    <p>Julienne Zhavala accidentally killed his best friend's father in self defense.</p>
-    <p>The courts would have easily acquitted him, had the event not revealed him to be hydrophin:
-    a siren-like species that evolved as a predator to humans.</p>
+    <p>Greatly changed by their lives apart, she invites him into a world outside the small-town terrors of their adolescence, where even a hydrophin raised in self-loathing may learn to be free.</p>
     <br />
-    <p>Though few of his kind maintain the activity in modern times, humans heavily stigmatize them,
-    and the community Julienne had only ever tried to love locked him away as a danger.</p>
+    <p>But Julienne wonders, isn’t it too late to start over?</p>
     <br />
-    <p>Fifteen years later, his childhood best friend, Opal Reyes, has finally managed to negotiate
-    his acquittal. Greatly changed by their lives apart, she invites him into a world outside the
-    small-town terrors of their adolescence, where even a hydrophin raised in self- loathing may
-    learn to be free.</p>
-    <br />
-    <p>But Julienne wonders, <br />isn't it too late to start over?</p>
-    <br />
-    <p>JULIENNE is a story of first days, of letting go of the narratives that bind us, and healing
-    as the sum of a thousand small things.</p>
+    <p>JULIENNE is a story of first days, of learning to let go of the narratives that bind us, and of healing as the sum of a thousand small things.</p>
     <br />
     <p>Also available to read on
       <a href="https://www.webtoons.com/en/challenge/julienne/list?title_no=806490">Webtoons</a> and 
       <a href="https://tapas.io/series/julienne">Tapas</a>!</p>
     <br />
-    <h3>About the Author</h3>
+    <h3>ABOUT THE AUTHOR</h3>
     <div class="about-the-author">
       <img id="portrait" src="/selfportrait2018.webp"/>
       <div>
-        <p><b>Sara</b> is a writer and illustrator passionate about the complicated journey of learning to
-        love and liberate our whole selves. She lives in the PNW with her old lady tuxedo cat and little 
-        brother ball python, and spends her free time bicycling, cooking, and spoiling her pets profusely.
-        This is her first graphic novel.</p>
+        <p><b>Sara</b> is a writer and illustrator passionate about the complicated journey of community and self-liberation. She lives in the Pacific Northwest with her old lady tuxedo cat and little brother ball python, and spends her free time bicycling, cooking, and spoiling her pets profusely.</p>
         <br />
         <p>She also writes prose! You can find her writing at her <a href="http://saranutter.wordpress.com/">
-          non-fiction blog</a>, or her <a href="https://thebioroboticist.com/2022/08/18/00/">new-adultscience
-          science fiction novel</a> about identity, belonging, and mad science, free to read online.</p>
+          non-fiction blog</a>, or her <a href="https://thebioroboticist.com/2022/08/18/00/">new-adult science
+          fiction novel</a> about identity, belonging, and biotech cults, free to read online.</p>
       </div>
     </div>
   </div>
