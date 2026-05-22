9ac140a23f7a60393e1c0873ec4e384b474b8299
pear <pear124@pm.me>
2026-05-22 10:21:58 -0700

    ensure scheduled branch is updated when immediate deploy occurs

--- scripts/pushToProd.sh
+++ scripts/pushToProd.sh
@@ -43,6 +43,7 @@ select yn in "Queue" "Prod"; do
   Prod)
     # Remote `prod` branch triggers GitHub Actions deploy (see .github/workflows).
     git push prod HEAD:prod
+    git push prod prod:main
     break
     ;;
   Queue)
